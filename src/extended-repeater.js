c44310e9f0a22f7f7ec0edd68ea718406282a21f
Yuri Savkov <yury.saukou@gmail.com>
2021-01-03 17:04:19 +0300

    feat: solve last tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  if (repeatTimes === undefined) {
+    return ''
+  }
+
+  let strReps = [];
+  const sepReps = [];
+
+  for (let i = 0; i < repeatTimes; i++) {
+    strReps.push(str);
+  }
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    sepReps.push(`${addition}`);
+  }
+
+  const addSepStr = sepReps.join(additionSeparator);
+
+  strReps = strReps.map((e) => e + addSepStr)
+  return strReps.join(separator);
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direct = true) {
+    this.direct = direct
+  }
+
+  encrypt(msg, key) {
+    /*cj = (mj + kj) % n*/
+
+    if (!msg || !key) {
+      throw Error('Check input parametrs!')
+    }
+
+    const spMsg = msg.toLowerCase().split('');
+    let spKey = key.toLowerCase();
+
+    while (spMsg.length > spKey.length) {
+      spKey += spKey;
+    }
+
+    spKey = spKey.split('');
+    let trigger = 0;
+    const res = [];
+
+    for (let l of spMsg) {
+      if (/[a-z]/i.test(l)) {
+        res.push(String.fromCharCode((((l.charCodeAt(0) - 97) + (spKey[trigger++].charCodeAt(0) - 97)) % 26) + 97).toUpperCase());
+      } else {
+        res.push(l)
+      }
+    }
+    
+    return this.direct ? res.join('') : res.reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(enMsg, key) {
+    /* mj = (cj + n - kj) % n */
+
+    if (!enMsg || !key) {
+      throw Error('Check input parametrs!')
+    }
+
+    const spEnMsg = enMsg.toLowerCase().split('');
+    let spKey = key.toLowerCase();
+
+    while (spEnMsg.length > spKey.length) {
+      spKey += spKey;
+    }
+
+    spKey = spKey.split('');
+    let trigger = 0;
+    const res = [];
+
+    for (let l of spEnMsg) {
+      if (/[a-z]/i.test(l)) {
+        res.push(String.fromCharCode((((l.charCodeAt(0) - 97) + (26 - (spKey[trigger++].charCodeAt(0) - 97))) % 26) + 97).toUpperCase());
+      } else {
+        res.push(l)
+      }
+    }
+    
+    return this.direct ? res.join('') : res.reverse().join('');
+
+
+    // String.fromCharCode(((('e'.charCodeAt(0) - 97) + (26 - ('l'.charCodeAt(0) - 97))) % 26) + 97);
   }
 }
 
